9d153cc9a8f3e44c5ed42dba63fd38346dc95abb
Jakov Prpic <jakov.prpic23@gmail.com>
2022-04-26 17:21:37 +0200

    migrations tasks, roles, applications, foreign keys added

---
+++ LV5/radovi/database/migrations/2022_04_26_121730_create_tasks_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->string('name',100);
+            $table->string('name_eng',100);
+            $table->string('description', 1000);
+            $table->string('study', 15);
+            $table->timestamps();
+        });
+
+        Schema::table('users', function(Blueprint $table){
+            $table->foreignId('task_id')->nullable()->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table){
+            $table->dropForeign(['task_id']);
+        });
+        Schema::dropIfExists('tasks');
+
+    }
+};
