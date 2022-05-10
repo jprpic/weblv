5601ee5db895dea52bd214c5a802d15e58081534
jPrpic <jakov.prpic23@gmail.com>
2022-05-10 17:46:19 +0200

    axios and cors setup

--- LV6/projects/projects.js
+++ LV6/projects/projects.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 4000
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
